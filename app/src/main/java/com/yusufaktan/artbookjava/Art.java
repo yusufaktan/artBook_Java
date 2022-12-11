7773b9b9f690ad576142f45c128d354b8734d270
Yusuf Aktan <ycan2497@gmail.com>
2022-12-11 17:06:19 +0300

    artBookJava

---
+++ app/src/main/java/com/yusufaktan/artbookjava/Art.java
@@ -0,0 +1,12 @@
+package com.yusufaktan.artbookjava;
+
+public class Art {
+
+    String name;
+    int id;
+
+    public Art(String name, int id) {
+        this.name = name;
+        this.id = id;
+    }
+}
